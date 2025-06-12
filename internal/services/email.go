945d0c258957c066548d72a2cebfba98c4f53cc4
princeprakhar <prakhardeep173@gmail.com>
2025-06-12 17:15:11 +0530

    updated the service/email.go to concetenate the token the url

--- internal/services/email.go
+++ internal/services/email.go
@@ -50,7 +50,7 @@ func (s *EmailService) SendProductUploadNotification(adminEmail, filePath string
 }
 
 func (s *EmailService) SendPasswordResetEmail(email, resetToken, baseURL string) error {
-	resetLink := fmt.Sprintf("%s/validate-token", baseURL)
+	resetLink := fmt.Sprintf("%s/validate-token/?token=%s", baseURL, resetToken)
 
 	subject := "Password Reset Request"
 	body := fmt.Sprintf(`
